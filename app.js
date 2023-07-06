895e6f6981d1cae83741e162a1ae5030e3170b3f
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-07-06 12:44:03 +0530

    create convertDateIntoString function

--- app.js
+++ app.js
@@ -9,3 +9,25 @@ function isPalindrome(str){
     let rev=reverseStr(str);
     return rev===str?true:false;
 }
+let date={
+    day:12,
+    month:2,
+    year:2022
+}
+function convertDateIntoString(date){
+    if (date.day<10) {
+        date.day='0'+date.day
+    }else{
+        date.day=toString(date.day)
+    }
+
+    if(date.month<10){
+        date.month='0'+date.month
+    }
+    else{
+        date.month=toString(date.month);
+    }
+
+    date.year=toString(date.year)
+    return date;
+}
