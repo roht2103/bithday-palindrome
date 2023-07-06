33e5095ffbde45b0e2cde13c502698c0aedc9702
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-07-06 15:03:01 +0530

    create getAllDateFormats function

--- app.js
+++ app.js
@@ -32,3 +32,22 @@ function convertDateIntoString(date){
     date.year=String(date.year)
     return date;
 }
+
+function getAllDateFormats(date){
+    convertDateIntoString(date);
+    let ddmmyyyy
+    let mmddyyyy
+    let yyyymmdd
+    let ddmmyy
+    let mmddyy
+    let yymmdd
+
+    ddmmyyyy=date.day+date.month+date.year;
+    mmddyyyy=date.month+date.day+date.year;
+    yyyymmdd=date.year+date.month+date.day
+    ddmmyy=date.day+date.month+date.year.slice(-2)
+    mmddyy=date.month+date.day+date.year.slice(-2)
+    yymmdd=date.year.slice(-2)+date.month+date.day
+
+    return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd]
+}
