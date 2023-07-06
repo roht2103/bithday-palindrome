b5b0443bc405e9acfe988c85913630db7f953c86
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-07-06 14:45:44 +0530

    Update: convertDateIntoString function

--- app.js
+++ app.js
@@ -18,16 +18,17 @@ function convertDateIntoString(date){
     if (date.day<10) {
         date.day='0'+date.day
     }else{
-        date.day=toString(date.day)
+        date.day=String(date.day)
+        
     }
 
     if(date.month<10){
         date.month='0'+date.month
     }
     else{
-        date.month=toString(date.month);
+        date.month=String(date.month);
     }
 
-    date.year=toString(date.year)
+    date.year=String(date.year)
     return date;
 }
